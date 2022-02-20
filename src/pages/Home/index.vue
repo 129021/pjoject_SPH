beba6e532d1a98c9e56592863280901fdf9bb9ec
129021 <wangjianqin1@yahoo.com>
2022-02-20 17:50:32 +0800

    day2:Home模块组件拆分

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>我是首页</div>
+  <div>
+    <!-- 三级联动的全局组件  已经注册为全局组件，不需要再引入了-->
+    <TypeNav/>
+  </div>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -12,37 +12,79 @@ import Search from '@/pages/Search'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 
+
+// 重写push和replace方法
+
+// 1. 先把VueRouter原型对象的Push,先保存一份
+let originPush = VueRouter.prototype.push;
+let originReplace = VueRouter.prototype.replace;
+
+// 2. 重写push和replace方法
+
+// push方法第一个参数：告诉原来的push方法，你往哪里跳转（传递哪些参数）
+// 第二个参数：成功的回调函数
+// 第三个参数：失败的回调函数
+VueRouter.prototype.push = function (location, resolve, reject) {
+    if (resolve && reject) {
+        //call/apply区别：
+        // 相同点：都可以调用函数一次，都可以篡改函数的上下文一次
+        // 不同点：call和apply传递参数：call传递参数用逗号隔开，apply方法执行，传递数组
+        originPush.call(this, location, resolve, reject);
+    } else {
+        originPush.call(this, location, () => {}, () => {})
+    }
+}
+
+VueRouter.prototype.replace = function (location, resolve, reject) {
+    if (resolve && reject) {
+        originReplace.call(this, location, resolve, reject)
+    } else {
+        originReplace.call(this, location, () => {}, () => {})
+    }
+}
+
+
+
+
+
 // 配置路由
 
 export default new VueRouter({
     // 配置路由
-    routes:[
-        {
-            path:'/home',
-            component:Home,
-            meta:{show:true}
+    routes: [{
+            path: '/home',
+            component: Home,
+            meta: {
+                show: true
+            }
         },
         {
-            path:'/search/:keyword',
-            component:Search,
-            meta:{show:true},
-            name:'search',
+            path: '/search/:keyword?',
+            component: Search,
+            meta: {
+                show: true
+            },
+            name: 'search',
         },
         {
-            path:'/login',
-            component:Login,
-            meta:{show:false}
+            path: '/login',
+            component: Login,
+            meta: {
+                show: false
+            }
         },
         {
-            path:'/register',
-            component:Register,
-            meta:{show:false}
+            path: '/register',
+            component: Register,
+            meta: {
+                show: false
+            }
         },
 
         // 重定向，在项目跑起来的时候，当访问/的时候，立马让他定向到首页
         {
-            path:'*',
-            redirect:'/home'
+            path: '*',
+            redirect: '/home'
 
         }
 

544d77aedf12d5564b116ebc361aa16975140ceb
129021 <wangjianqin1@yahoo.com>
2022-02-23 09:53:30 +0800

    day2：has finished

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,5 +1,5 @@
 // home 模块的小仓库
-import {reqCategoryList} from '@/api'
+import {reqCategoryList} from '@/api';
 
 
 const state = {
@@ -8,7 +8,9 @@ const state = {
 };
 const mutations = {
     CATEGORYLIST(state,categoryList){
-        state.categoryList=categoryList
+
+        // 这里应该有16条数据，但是接口数据被人修改变成了17条，为了能够展示，这里只取前16条
+        state.categoryList=categoryList.slice(0,16)
     }
 };
 const actions = {
@@ -16,7 +18,7 @@ const actions = {
    async categoryList({commit}){
         let result =await reqCategoryList();
         // console.log(result);
-        if(result.code==20){
+        if(result.code==200){
             commit("CATEGORYLIST",result.data)
         }
 
